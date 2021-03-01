709680ed1223be2cd552bf6141a46307177f29fa
MEMET AVCI <72238534+MEHMET0665@users.noreply.github.com>
2021-02-28 22:59:19 -0500

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports=()=>{
-  mongoose.connect('mongodb+srv://meavci:ZV9c7m9aSpltSunQ@cluster0.5uqz0.mongodb.net/movieapp?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect('mongodb+srv://meavci:<password>@cluster0.5uqz0.mongodb.net/movieapp?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.on('open',()=>{console.log('MongoDB Connected success')})
 mongoose.connection.on('error',(err)=>{console.log('MongoDB: Connection failed:',err)})
 }
